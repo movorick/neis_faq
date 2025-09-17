8ce182085dba1ab4e718e8d673e0f3c249540144
movorick <movorick@gmail.com>
2025-09-18 08:19:48 +0900

    Add files via upload

---
+++ faq.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
+import retriever
+import os
+from dotenv import load_dotenv
+load_dotenv()
+OPEN_API_KEY=os.getenv('OPEN_API_KEY')
+
+# 모델 초기화
+llm = ChatOpenAI(model="gpt-4o-mini",api_key=OPEN_API_KEY)
+
+# 사용자의 메시지 처리하기 위한 함수
+def get_ai_response(messages, docs):    
+    response = retriever.document_chain.stream({
+        "messages": messages,
+        "context": docs
+    })
+
+    for chunk in response:
+        yield chunk
+
+# Streamlit 앱
+st.set_page_config(
+    page_title='제주특별자치도교육청 챗봇',
+    page_icon='💬',
+    layout='wide'
+)
+st.title("💬 제주특별자치도교육청 챗봇")
+
+# 스트림릿 session_state에 메시지 저장
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        SystemMessage("벡터스토어에서 검색해서 알맞은 답변을 해줘 "),  
+        AIMessage("무엇을 도와 드릴까요?")
+    ]
+
+# 스트림릿 화면에 메시지 출력
+for msg in st.session_state.messages:
+    if msg.content:
+        if isinstance(msg, AIMessage):
+            st.chat_message("assistant").write(msg.content)
+        elif isinstance(msg, HumanMessage):
+            st.chat_message("user").write(msg.content)
+
+# 사용자 입력 처리
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt) # 사용자 메시지 출력
+    st.session_state.messages.append(HumanMessage(prompt)) # 사용자 메시지 저장
+
+    augmented_query = retriever.query_augmentation_chain.invoke({
+        "messages": st.session_state["messages"],
+        "query": prompt,
+    })
+    print("augmented_query\t", augmented_query)
+    # 관련 문서 검색
+    print("관련 문서 검색")
+    docs = retriever.retriever.invoke(f"{prompt}\n{augmented_query}")
+    for doc in docs:
+        print('--------------')
+        print(doc)
+    print('===================')
+
+    with st.spinner(f"AI가 답변을 준비 중입니다... '{augmented_query}'"):
+        response = get_ai_response(st.session_state["messages"],docs)
+        result = st.chat_message("assistant").write_stream(response) # AI 메시지 출력
+    st.session_state["messages"].append(AIMessage(result)) # AI 메시지 저장    
