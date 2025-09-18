91bd6bf56bb00e4650c339a025b8d60ba9e5b9e8
movorick <movorick@gmail.com>
2025-09-18 23:31:55 +0900

    Update faq.py

--- faq.py
+++ faq.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
-import faq_retriever
+import faq_retriever as retriever
 import os
 
 # 모델 초기화
@@ -62,5 +62,6 @@ if prompt := st.chat_input():
         response = get_ai_response(st.session_state["messages"],docs)
         result = st.chat_message("assistant").write_stream(response) # AI 메시지 출력
     st.session_state["messages"].append(AIMessage(result)) # AI 메시지 저장    
+
 
 
