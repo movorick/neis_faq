568a00d9ebc08f044b62ffad3e6af9e4dbf8a652
movorick <movorick@gmail.com>
2025-09-18 14:41:01 +0900

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -3,6 +3,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
+from langchain_chroma import Chroma
 load_dotenv()
 OPEN_API_KEY=os.getenv('OPEN_API_KEY')
 embedding = OpenAIEmbeddings(model='text-embedding-3-large',api_key=OPEN_API_KEY)
@@ -63,6 +64,7 @@ query_augmentation_prompt = ChatPromptTemplate.from_messages(
 )
 
 query_augmentation_chain = query_augmentation_prompt |llm| StrOutputParser()
+
 
 
 
